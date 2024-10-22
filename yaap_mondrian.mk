61013d093de039574dc28d371e949fd543127d61
Keosh <106485828+keosh1@users.noreply.github.com>
2024-10-22 12:22:36 +0300

    merge Device identifier to come after all inclusions

--- yaap_mondrian.mk
+++ yaap_mondrian.mk
@@ -16,6 +16,23 @@ TARGET_BUILD_GAPPS := true
 # MIUI Camera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera-mondrian/miuicamera-mondrian-vendor.mk)
 
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#    PRIVATE_BUILD_DESC="mondrian_global-user 14 UKQ1.230804.001 V816.0.8.0.UMNMIXM release-keys" \
+#    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+#    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.8.0.UMNMIXM:user/release-keys
+
+TARGET_HAS_UDFPS := true
+
+EXTRA_UDFPS_ANIMATIONS := true
+
+TARGET_ENABLE_BLUR := true
+
+PRODUCT_NO_CAMERA := false
+
+# Device identifier. This must come after all inclusions.
+
 PRODUCT_NAME := yaap_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
@@ -25,19 +42,6 @@ PRODUCT_MODEL := 23013PC75G
 PRODUCT_SYSTEM_NAME := mondrian_global
 PRODUCT_SYSTEM_DEVICE := mondrian
 
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRIVATE_BUILD_DESC="mondrian_global-user 14 UKQ1.230804.001 V816.0.8.0.UMNMIXM release-keys" \
-#    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-#    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.8.0.UMNMIXM:user/release-keys
+PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-TARGET_HAS_UDFPS := true
-
-EXTRA_UDFPS_ANIMATIONS := true
-
-TARGET_ENABLE_BLUR := true
-
-PRODUCT_NO_CAMERA := false

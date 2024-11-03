7066f3ec1b44fc147e9a031e496dd3c6e8de4d8f
Keosh <106485828+keosh1@users.noreply.github.com>
2024-11-03 03:27:57 +0200

    Revert "remove miui camera path and add some flags"
    This reverts commit 2064fd15ce69aebcb02c68c25bbe7f3695c4a55e.

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -7,9 +7,8 @@
 # Inherit from mondrian device
 $(call inherit-product, device/xiaomi/mondrian/device.mk)
 
-TARGET_DISABLE_EPPE := true
-TARGET_SUPPORTS_64_BIT_APPS := true
-TARGET_BOOT_ANIMATION_RES := 1080
+# MIUI Camera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-mondrian/miuicamera-mondrian-vendor.mk)
 
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)

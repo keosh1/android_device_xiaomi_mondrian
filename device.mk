85079cba382d1c239dea735e5421830cf5a9480f
Keosh <106485828+keosh1@users.noreply.github.com>
2024-11-19 19:03:32 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/mondrian/mondrian-vendor.mk)
-$(call inherit-product, vendor/xiaomi/miuicamera-mondrian/miuicamera-mondrian-vendor.mk)
+$(call inherit-product, vendor/xiaomi/miuicamera-mondrian/miuicamera-vendor.mk)
 
 # Overlay
 PRODUCT_PACKAGES += \

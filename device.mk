8e1e8e9a3b3f055d6286758160d8bec0f15584af
Keosh <106485828+keosh1@users.noreply.github.com>
2024-11-19 18:46:37 +0200

    miui camera 2

--- device.mk
+++ device.mk
@@ -13,9 +13,7 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/mondrian/mondrian-vendor.mk)
-
-# Call the MiuiCamera setup
-$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+$(call inherit-product, vendor/xiaomi/miuicamera-mondrian/miuicamera-mondrian-vendor.mk)
 
 # Overlay
 PRODUCT_PACKAGES += \

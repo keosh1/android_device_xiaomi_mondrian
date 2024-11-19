195ee0345f56716b2eda9393c11e9cb27dea5858
Keosh <106485828+keosh1@users.noreply.github.com>
2024-11-19 17:24:51 +0200

    Update miuicamera inherit path

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 $(call inherit-product, vendor/xiaomi/mondrian/mondrian-vendor.mk)
 
 # Call the MiuiCamera setup
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Overlay
 PRODUCT_PACKAGES += \
